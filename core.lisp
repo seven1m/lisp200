f5349f3fc9b5f5ad9c06ee36fbf9153edb7539eb
Tim Morgan <tim@timmorgan.org>
2019-05-08 20:48:41 -0500

    Code style

--- core.lisp
+++ core.lisp
@@ -1,25 +1,25 @@
-(def cons (fn (a b) (. (. Array "[]" a) "+" (if b b (. Array "new")))))
-(def concat (fn (a b) (. a "+" b)))
+(def cons (fn [a b] (. (. Array "[]" a) "+" (if b b (. Array "new")))))
+(def concat (fn [a b] (. a "+" b)))
 
-(def = (fn (a b) (. a "==" b)))
-(def < (fn (a b) (. a "<" b)))
-(def <= (fn (a b) (. a "<=" b)))
-(def > (fn (a b) (. a ">" b)))
-(def >= (fn (a b) (. a ">=" b)))
+(def = (fn [a b] (. a "==" b)))
+(def < (fn [a b] (. a "<" b)))
+(def <= (fn [a b] (. a "<=" b)))
+(def > (fn [a b] (. a ">" b)))
+(def >= (fn [a b] (. a ">=" b)))
 
-(def nil? (fn (a) (. a "nil?")))
-(def throw (fn (a) (raise a)))
+(def nil? (fn [a] (. a "nil?")))
+(def throw (fn [a] (raise a)))
 
-(def length (fn (a) (if a (. a "length") 0)))
-(def empty? (fn (a) (= 0 (length a))))
-(def list (fn (& args) args))
-(def range (fn (first last) (. Range "new" first last)))
-(def first (fn (a) (. a "first")))
-(def rest (fn (a) (if (< (length a) 2) (list) (. a "[]" (range 1 -1)))))
-(def last (fn (a) (. a "last")))
+(def length (fn [a] (if a (. a "length") 0)))
+(def empty? (fn [a] (= 0 (length a))))
+(def list (fn [& args] args))
+(def range (fn [first last] (. Range "new" first last)))
+(def first (fn [a] (. a "first")))
+(def rest (fn [a] (if (< (length a) 2) (list) (. a "[]" (range 1 -1)))))
+(def last (fn [a] (. a "last")))
 
 (defmacro and
-  (fn (& args)
+  (fn [& args]
       (if (empty? args)
         true
         (if (= 1 (length args))
@@ -29,7 +29,7 @@
              ,(first args))))))
 
 (defmacro or
-  (fn (& args)
+  (fn [& args]
       (if (empty? args)
         false
         (if (= 1 (length args))
@@ -39,54 +39,54 @@
              (or ,@(rest args)))))))
 
 (def reduce
-     (fn (f init coll)
+     (fn [f init coll]
          (. coll "inject" (block f))))
 
 (def filter
-     (fn (pred a)
+     (fn [pred a]
          (if (empty? a)
            nil
            (if (pred (first a))
              (cons (first a) (filter pred (rest a)))
              (filter pred (rest a))))))
 
-(def * (fn (& args)
+(def * (fn [& args]
            (or
-             (reduce (fn (t n) (. t "*" n)) 1 args)
+             (reduce (fn [t n] (. t "*" n)) 1 args)
              1)))
-(def / (fn (& args)
+(def / (fn [& args]
            (if (= 1 (length args))
              (. 1 "/" (. (first args) "to_f"))
              (. args "inject"
-                (block (fn (t n) (. t "/" (. n "to_f"))))))))
-(def + (fn (& args)
+                (block (fn [t n] (. t "/" (. n "to_f"))))))))
+(def + (fn [& args]
            (.
              (. args "inject"
-                (block (fn (t n) (. t "+" n))))
+                (block (fn [t n] (. t "+" n))))
              "to_i")))
-(def - (fn (& args)
+(def - (fn [& args]
            (if (= 1 (length args))
              (* -1 (first args))
              (.
-               (reduce (fn (t n) (. t "-" n)) 0 args)
+               (reduce (fn [t n] (. t "-" n)) 0 args)
                "to_i"))))
 
-(def print (fn (a) (. Kernel "print" (. a "to_s"))))
-(def println (fn (a) (print a) (print "\n")))
-(defmacro not (fn (a) (if a false true)))
-(defmacro unless (fn (pred t f) `(if ,pred ,f ,t)))
+(def print (fn [a] (. Kernel "print" (. a "to_s"))))
+(def println (fn [a] (print a) (print "\n")))
+(defmacro not (fn [a] (if a false true)))
+(defmacro unless (fn [pred t f] `(if ,pred ,f ,t)))
 (def str
-     (fn (& args)
+     (fn [& args]
          (if (empty? args)
            ""
            (.
              (. (first args) "to_s")
              "+"
              (apply str (rest args))))))
-(def even? (fn (a) (. a "even?")))
-(def odd? (fn (a) (. a "odd?")))
+(def even? (fn [a] (. a "even?")))
+(def odd? (fn [a] (. a "odd?")))
 
-;(defmacro let (fn (binds & body)
+;(defmacro let (fn [binds & body]
 ;(def binds nil)
 ;(def vals nil)
 ;(:
@@ -96,7 +96,7 @@
 ;)))
 
 (defmacro assert
-  (fn (pred message)
+  (fn [pred message]
       `(if ,pred
          (print ".")
          (println
@@ -106,7 +106,7 @@
                ,message
                (str "Expected " (. ,pred "inspect") " to be truthy")))))))
 (defmacro assert=
-  (fn (expected actual message)
+  (fn [expected actual message]
       `(if (= ,expected ,actual)
          (print ".")
          (println
